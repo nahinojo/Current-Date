0bd6b6c5bd6e36f56b803fd74c28881fb956f957
Noah Hinojos <noah.hinojos@gmail.com>
2022-08-31 13:12:19 -0700

    Fixed multiple spans with same id.

--- time.js
+++ time.js
@@ -48,7 +48,8 @@ function updateClock () {
     document.getElementById('hour').innerHTML = hour;
     document.getElementById('min').innerHTML = min;
     document.getElementById('sec').innerHTML = sec;
-    document.getElementById('colon').innerHTML = ':';
+    document.getElementById('colon-hm').innerHTML = ':';
+    document.getElementById('colon-ms').innerHTML = ':';
     document.getElementById('comma').innerHTML = ', ';
 }
 
